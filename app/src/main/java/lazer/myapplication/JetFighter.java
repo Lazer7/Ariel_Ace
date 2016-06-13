b3a52790a99f663c7cac67986546e3874e6975c5
Jimmy <iamjimmyjimmy@yahoo.com>
2016-06-12 19:32:55 -0700

    Set some methods for creation
    Set some methods for creation

--- app/src/main/java/lazer/myapplication/JetFighter.java
+++ app/src/main/java/lazer/myapplication/JetFighter.java
@@ -4,4 +4,16 @@ package lazer.myapplication;
  * Created by Jimmy on 6/12/2016.
  */
 public class JetFighter {
+    /**Get the activity that has the jetfighter**/
+    /**
+     * Gets the image from the main activity
+     * and set default weapon choice
+     */
+    public JetFighter(){}
+
+    /**
+     * Get the user's choice of weapon
+     * from the customize activity
+     */
+    public void weapontype(){}
 }
