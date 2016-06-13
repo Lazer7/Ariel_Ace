f02111bc4900444ee02435aa9d339263d409d6b6
Jimmy <iamjimmyjimmy@yahoo.com>
2016-06-12 19:21:27 -0700

    Level Selection Screen
    We have added the Selection Menu which is accessed by the Play button,
    currently we have 9 planet stages and hoping to add more...

---
+++ app/src/main/java/lazer/myapplication/LevelSelectionScreen.java
@@ -0,0 +1,11 @@
+package lazer.myapplication;
+
+import android.os.Bundle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_selection_screen);
+    }
+}
