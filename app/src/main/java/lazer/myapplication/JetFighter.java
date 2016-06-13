ad653d79e3b8ac5aef7eb43ce22a46bb48235d1f
James <iamjimmyjimmy@yahoo.com>
2016-06-12 19:42:00 -0700

    Jetfighterwork (#8)
    * JetFighterClass
    
    The appropriate Jet Fighter Methods and image rendering for the Jet
    Fighter object
    
    * Set some methods for creation
    
    Set some methods for creation
    
    * Testing out JetFighterBranch
    
    * Testing JetFighter Branch
    
    * Testing the JetFighter branch

--- app/src/main/java/lazer/myapplication/JetFighter.java
+++ app/src/main/java/lazer/myapplication/JetFighter.java
@@ -4,4 +4,21 @@ package lazer.myapplication;
  * Created by Jimmy on 6/12/2016.
  */
 public class JetFighter {
+    /**Get the activity that has the jetfighter**/
+    /**
+     * Gets the image from the main activity
+     * and set default weapon choice
+     */
+    public JetFighter(){
+        //make your own parameters
+    }
+
+    /**
+     * Get the user's choice of weapon
+     * from the customize activity
+     */
+    public void weapontype(){
+        //make weapons at the customization menu
+    }
+
 }
