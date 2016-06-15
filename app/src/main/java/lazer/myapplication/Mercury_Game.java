b981ecf9d2b663d42b78574d1bfe540a3f42661e
Jimmy <iamjimmyjimmy@yahoo.com>
2016-06-14 21:12:45 -0700

    Mercury Level Creation
    Added files for mercury level and added a ship png

---
+++ app/src/main/java/lazer/myapplication/Mercury_Game.java
@@ -0,0 +1,13 @@
+package lazer.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mercury_Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mercury__game);
+    }
+}
